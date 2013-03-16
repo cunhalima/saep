b0f6b1f1b3ec77fcf9f53ce289fd371f7465eaaf
Alex Reimann Cunha Lima <reimanncl@gmail.com>
2013-03-16 10:54:58 +0200

    teste 1

--- public/index.php
+++ public/index.php
@@ -14,5 +14,45 @@ SAEP_printStatus();
 </ul>
 
 <?php
+function test() {
+    if ($_SERVER['DOCUMENT_ROOT'] === 'D:/xampp/htdocs') {
+        $hostname = 'localhost';
+        $password = 'password';
+    } else {
+        $hostname = 'saep-db.my.phpcloud.com';
+        $password = '7f3iYhGMhg3w';
+    }
+    // connect to the database
+    //$con = mysql_connect('saep-db.my.phpcloud.com','saep','7f3iYhGMhg3w') 
+    //$con = mysql_connect('localhost','saep','password') 
+    $con = mysql_connect($hostname, 'saep', $password) 
+    or die('Could not connect to the server!');
+    // select a database:
+    mysql_select_db('saep') 
+        or die('Could not select a database.');
+    /*
+    ** Fetch some rows from database:
+    */
+
+    // read username from URL
+    //$username = $_GET['username'];
+    // escape bad chars:
+    //$username = mysql_real_escape_string($username);
+    // build query:
+    $sql = "select * from usuario";
+    // execute query:
+    $result = mysql_query($sql) 
+        or die('A error occured: ' . mysql_error());
+    // get result count:
+    $count = mysql_num_rows($result);
+    print "Showing $count rows:<hr/>";
+    // fetch results:
+    while ($row = mysql_fetch_assoc($result)) {
+        $row_nome = $row['nome'];
+        $row_senha = $row['senha'];
+        print "$row_nome = $row_senha<br />";
+    }
+}
+test();
 HTML_footer();
 ?>
